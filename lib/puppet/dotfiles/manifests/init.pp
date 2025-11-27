6bac127adb4e1489b43968c5a2d5f31747617b50
Joyful Lee <git@joyfullee.me>
2025-11-26 22:29:33 -0500

    Switch back to alacritty
    Foot is incredibly slow on Raspberry Pi and urxvt is showing its age.
    Meanwhile, alacritty continues to improve including on previously
    sluggish platforms like Pinebook Pro.

--- lib/puppet/dotfiles/manifests/init.pp
+++ lib/puppet/dotfiles/manifests/init.pp
@@ -1,12 +1,5 @@
 class dotfiles {
-  if $facts['profile'] and $facts['profile']['platform'] == 'pinebookpro' {
-    file { "${facts['basedir']}/.config/systemd/user/x-session.target.wants/urxvtd.service":
-      ensure => absent,
-    }
-  } else {
-    file { "${facts['basedir']}/.config/systemd/user/x-session.target.wants/urxvtd.service":
-      ensure => link,
-      target => '../urxvtd.service',
-    }
+  file { "${facts['basedir']}/.config/systemd/user/x-session.target.wants/urxvtd.service":
+    ensure => absent,
   }
 }
