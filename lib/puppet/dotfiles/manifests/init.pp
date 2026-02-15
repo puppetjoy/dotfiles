16700996d7369a867807b660034795106cc53336
Joyful Lee <git@joyfullee.me>
2026-02-15 15:24:45 -0500

    windows-terminal: add Cygwin zsh profile managed by Puppet

--- lib/puppet/dotfiles/manifests/init.pp
+++ lib/puppet/dotfiles/manifests/init.pp
@@ -2,4 +2,37 @@ class dotfiles {
   file { "${facts['basedir']}/.config/systemd/user/x-session.target.wants/urxvtd.service":
     ensure => absent,
   }
+
+  if $facts['os']['family'] == 'windows' {
+    $localappdata = $facts['env_localappdata'] ? {
+      undef   => "C:/Users/${facts['identity']['user']}/AppData/Local",
+      default => $facts['env_localappdata'],
+    }
+
+    $windows_terminal_source = "${facts['basedir']}/.config/windows-terminal/settings.json"
+    $windows_terminal_paths = [
+      "${localappdata}/Microsoft/Windows Terminal/settings.json",
+      "${localappdata}/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json",
+      "${localappdata}/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json",
+    ]
+    $windows_terminal_parent_dirs = [
+      "${localappdata}/Microsoft/Windows Terminal",
+      "${localappdata}/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState",
+      "${localappdata}/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState",
+    ]
+
+    file { $windows_terminal_parent_dirs:
+      ensure => directory,
+    }
+
+    file { $windows_terminal_paths:
+      ensure  => file,
+      source  => $windows_terminal_source,
+      replace => true,
+      require => [
+        File[$windows_terminal_source],
+        File[$windows_terminal_parent_dirs],
+      ],
+    }
+  }
 }
