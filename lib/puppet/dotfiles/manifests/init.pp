8d91e8c54f9b6c21b11391eef2e82a8bc2cb0678
Joyful Lee <git@joyfullee.me>
2026-02-15 15:29:15 -0500

    dotfiles: fix Windows Terminal path handling on Puppet

--- lib/puppet/dotfiles/manifests/init.pp
+++ lib/puppet/dotfiles/manifests/init.pp
@@ -4,10 +4,14 @@ class dotfiles {
   }
 
   if $facts['os']['family'] == 'windows' {
-    $localappdata = $facts['env_localappdata'] ? {
-      undef   => "C:/Users/${facts['identity']['user']}/AppData/Local",
+    # identity.user may include a machine/domain prefix on Windows (for
+    # example "CARACARA\\joy"), so trim everything up to the final slash.
+    $windows_username = regsubst($facts['identity']['user'], '.*[\\\\/]', '')
+    $localappdata_raw = $facts['env_localappdata'] ? {
+      undef   => "C:/Users/${windows_username}/AppData/Local",
       default => $facts['env_localappdata'],
     }
+    $localappdata = regsubst($localappdata_raw, '\\\\', '/', 'G')
 
     $windows_terminal_source = "${facts['basedir']}/.config/windows-terminal/settings.json"
     $windows_terminal_paths = [
@@ -16,8 +20,13 @@ class dotfiles {
       "${localappdata}/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json",
     ]
     $windows_terminal_parent_dirs = [
+      "${localappdata}",
+      "${localappdata}/Microsoft",
       "${localappdata}/Microsoft/Windows Terminal",
+      "${localappdata}/Packages",
+      "${localappdata}/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe",
       "${localappdata}/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState",
+      "${localappdata}/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe",
       "${localappdata}/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState",
     ]
 
